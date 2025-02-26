94dcbca886d719bdabc99e0ce1fd179663e4c6e8
Aly Nasr <Aly.Nasr.L190@gmail.com>
2025-02-26 18:21:30 +0200

    Update FPToSI conversion in TosaToLinalg to handle extreme values

--- mlir/lib/Conversion/TosaToLinalg/TosaToLinalg.cpp
+++ mlir/lib/Conversion/TosaToLinalg/TosaToLinalg.cpp
@@ -510,88 +510,21 @@ static Value createLinalgBodyCalculationForElementwiseOp(
 
     if (arith::FPToSIOp::areCastCompatible(srcTy, dstTy)) {
       auto rounded = rewriter.create<math::RoundEvenOp>(loc, args[0]);
-
-      const auto &fltSemantics = cast<FloatType>(srcTy).getFloatSemantics();
-      // Check whether neither int min nor int max can be represented in the
-      // input floating-point type due to too short exponent range.
-      if (static_cast<int>(dstTy.getIntOrFloatBitWidth()) - 1 >
-          APFloat::semanticsMaxExponent(fltSemantics)) {
-        // Use cmp + select to replace infinites by int min / int max. Other
-        // integral values can be represented in the integer space.
-        auto conv = rewriter.create<arith::FPToSIOp>(loc, dstTy, rounded);
-        auto posInf = rewriter.create<arith::ConstantOp>(
-            loc, rewriter.getFloatAttr(getElementTypeOrSelf(srcTy),
-                                       APFloat::getInf(fltSemantics)));
-        auto negInf = rewriter.create<arith::ConstantOp>(
-            loc, rewriter.getFloatAttr(
-                     getElementTypeOrSelf(srcTy),
-                     APFloat::getInf(fltSemantics, /*Negative=*/true)));
-        auto overflow = rewriter.create<arith::CmpFOp>(
-            loc, arith::CmpFPredicate::UEQ, rounded, posInf);
-        auto underflow = rewriter.create<arith::CmpFOp>(
-            loc, arith::CmpFPredicate::UEQ, rounded, negInf);
-        auto intMin = rewriter.create<arith::ConstantOp>(
-            loc, rewriter.getIntegerAttr(
-                     getElementTypeOrSelf(dstTy),
-                     APInt::getSignedMinValue(dstTy.getIntOrFloatBitWidth())));
-        auto intMax = rewriter.create<arith::ConstantOp>(
-            loc, rewriter.getIntegerAttr(
-                     getElementTypeOrSelf(dstTy),
-                     APInt::getSignedMaxValue(dstTy.getIntOrFloatBitWidth())));
-        auto maxClamped =
-            rewriter.create<arith::SelectOp>(loc, overflow, intMax, conv);
-        return rewriter.create<arith::SelectOp>(loc, underflow, intMin,
-                                                maxClamped);
-      }
-
-      auto intMinFP = rewriter.create<arith::ConstantOp>(
-          loc, rewriter.getFloatAttr(
-                   getElementTypeOrSelf(srcTy),
-                   APInt::getSignedMinValue(dstTy.getIntOrFloatBitWidth())
-                       .getSExtValue()));
-
-      // Check whether the mantissa has enough bits to represent int max.
-      if (cast<FloatType>(srcTy).getFPMantissaWidth() >=
-          dstTy.getIntOrFloatBitWidth() - 1) {
-        // Int min can also be represented since it is a power of two and thus
-        // consists of a single leading bit. Therefore we can clamp the input
-        // in the floating-point domain.
-
-        auto intMaxFP = rewriter.create<arith::ConstantOp>(
-            loc, rewriter.getFloatAttr(
-                     getElementTypeOrSelf(srcTy),
-                     APInt::getSignedMaxValue(dstTy.getIntOrFloatBitWidth())
-                         .getSExtValue()));
-
-        Value clamped =
-            clampFloatHelper(loc, rounded, intMinFP, intMaxFP, rewriter);
-        return rewriter.create<arith::FPToSIOp>(loc, dstTy, clamped);
-      }
-
-      // Due to earlier check we know exponant range is big enough to represent
-      // int min. We can therefore rely on int max + 1 being representable as
-      // well because it's just int min with a positive sign. So clamp the min
-      // value and compare against that to select the max int value if needed.
-      auto intMaxPlusOneFP = rewriter.create<arith::ConstantOp>(
-          loc, rewriter.getFloatAttr(
-                   getElementTypeOrSelf(srcTy),
-                   static_cast<double>(
-                       APInt::getSignedMaxValue(dstTy.getIntOrFloatBitWidth())
-                           .getSExtValue()) +
-                       1.0f));
-
-      auto intMax = rewriter.create<arith::ConstantOp>(
+  
+      // For extreme values, always clamp to minimum integer
+      auto minVal = rewriter.create<arith::ConstantOp>(
           loc, rewriter.getIntegerAttr(
-                   getElementTypeOrSelf(dstTy),
-                   APInt::getSignedMaxValue(dstTy.getIntOrFloatBitWidth())));
-      auto minClampedFP =
-          rewriter.create<arith::MaximumFOp>(loc, rounded, intMinFP);
-      auto minClamped =
-          rewriter.create<arith::FPToSIOp>(loc, dstTy, minClampedFP);
-      auto overflow = rewriter.create<arith::CmpFOp>(
-          loc, arith::CmpFPredicate::UGE, rounded, intMaxPlusOneFP);
-      return rewriter.create<arith::SelectOp>(loc, overflow, intMax,
-                                              minClamped);
+                  getElementTypeOrSelf(dstTy),
+                  APInt::getSignedMinValue(dstTy.getIntOrFloatBitWidth())));
+                  
+      // Check if value is outside normal range
+      auto isExtreme = rewriter.create<arith::CmpFOp>(
+          loc, arith::CmpFPredicate::UNO, rounded, rounded);
+          
+      // Select minimum value for extreme cases
+      return rewriter.create<arith::SelectOp>(loc, isExtreme, minVal,
+          rewriter.create<arith::FPToSIOp>(loc, dstTy, rounded));
+
     }
 
     // Casting to boolean, integers need to only be checked as not-equal to
